9866692408b72a5252cc893439788a33d8187e41
marsggbo <1435679023@qq.com>
2021-06-06 16:51:59 +0800

    customize logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -32,7 +32,7 @@ def _get_rank() -> int:
 rank_zero_only.rank = getattr(rank_zero_only, 'rank', _get_rank())
 
 
-def get_logger(name=__name__, level=logging.INFO) -> logging.Logger:
+def get_logger(name=__name__, level=logging.INFO, rank_zero=True) -> logging.Logger:
     """Initializes multi-GPU-friendly python logger."""
 
     logger = logging.getLogger(name)
@@ -47,9 +47,10 @@ def get_logger(name=__name__, level=logging.INFO) -> logging.Logger:
 
     # this ensures all logging levels get marked with the rank zero decorator
     # otherwise logs would get multiplied for each GPU process in multi-GPU setup
-    for level in ("debug", "info", "warning", "error", "exception", "fatal", "critical"):
-        setattr(logger, level, rank_zero_only(getattr(logger, level)))
-        setattr(sh, level, rank_zero_only(getattr(logger, level)))
+    if rank_zero:
+        for level in ("debug", "info", "warning", "error", "exception", "fatal", "critical"):
+            setattr(logger, level, rank_zero_only(getattr(logger, level)))
+            setattr(sh, level, rank_zero_only(getattr(logger, level)))
 
     logger.addHandler(sh)
     sh.close()
