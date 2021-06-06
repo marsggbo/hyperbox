932bde3ad5a41672d9a009dd09d49365f94259c5
marsggbo <1435679023@qq.com>
2021-06-06 17:02:31 +0800

    add knowledge distillation loss function

---
+++ src/losses/kd_loss.py
@@ -0,0 +1,16 @@
+import torch.nn.functional as F
+
+
+def KDLoss(outputs, teacher_outputs, alpha=0.4, temperature=2):
+    """
+    Compute the knowledge-distillation (KD) loss given outputs, labels.
+    "Hyperparameters": temperature and alpha
+    NOTE: the KL Divergence for PyTorch comparing the softmaxs of teacher
+    and student expects the input tensor to be log probabilities! See Issue #2
+    """
+    T = temperature
+    student = F.log_softmax(outputs/T, dim=1)
+    teacher = F.softmax(teacher_outputs/T, dim=1)
+    KD_loss = F.kl_div(student, teacher)* (alpha * T * T)
+
+    return KD_loss
