54f0c894a4fc45c40df47246a5020b11dde8c4ce
marsggbo <1435679023@qq.com>
2021-06-10 19:19:07 +0800

    update run script

--- run.sh
+++ run.sh
@@ -1,21 +1,36 @@
+#!/bin/bash
+
 num=$1
-BS=32
+remark=$2
+others=$3
+
+BS=128
 exp=example_ofa_nas
-if [ $num -eq 1 ]
+
+
+if [ $num -le 1 ]
 then
 {  
-    echo "python run.py experiment=$exp.yaml logger.wandb.name=g${num}_bs${BS} trainer.gpus=${num} datamodule.batch_size=${BS}"
-    python run.py experiment=$exp.yaml logger.wandb.name=g${num}_bs${BS} trainer.gpus=${num} datamodule.batch_size=${BS}
-}
-elif [ $num -le 4 ]
-then
-{
-    echo "python run.py experiment=$exp.yaml logger.wandb.name=ddp_g${num}_bs${BS} trainer.gpus=${num} trainer.accelerator=ddp datamodule.batch_size=${BS}"
-    python run.py experiment=$exp.yaml logger.wandb.name=ddp_g${num}_bs${BS} trainer.gpus=${num} trainer.accelerator=ddp datamodule.batch_size=${BS}
+    echo "python run.py experiment=$exp.yaml logger.wandb.name=g${num}_bs${BS}_${remark} trainer.gpus=${num} datamodule.batch_size=${BS} ${others}"
+    python run.py \
+    experiment=$exp.yaml \
+    logger.wandb.name=g${num}_bs${BS}_${remark} \
+    trainer.gpus=${num} \
+    datamodule.batch_size=${BS} \
+    logger.wandb.offline=True \
+    $others
 }
 else
 {
-    echo "#gpus should less than 5"    
+    echo "python run.py experiment=$exp.yaml logger.wandb.name=ddp_g${num}_bs${BS}_${remark} trainer.gpus=${num} trainer.accelerator=ddp datamodule.batch_size=${BS} ${others}"
+    python run.py \
+    experiment=$exp.yaml \
+    logger.wandb.name=ddp_g${num}_bs${BS}_${remark} \
+    trainer.gpus=${num} \
+    trainer.accelerator=ddp \
+    datamodule.batch_size=${BS} \
+    logger.wandb.offline=True \
+    $others
 }
 # mpirun -np ${num} python run.py experiment=$exp.yaml logger.wandb.name=ddp_g${num} trainer.gpus=1 trainer.accelerator=horovod
 fi
