2c5eb3e3da331b77349347c477a81bb5022bcbd2
marsggbo <1435679023@qq.com>
2021-06-21 20:33:57 +0800

    add Zero_layer to DARTS ops

--- hyperbox/networks/darts/darts_ops.py
+++ hyperbox/networks/darts/darts_ops.py
@@ -194,3 +194,23 @@ class FactorizedReduce(nn.Module):
         out = torch.cat([self.conv1(x), self.conv2(x[:, :, 1:, 1:])], dim=1)
         out = self.bn(out)
         return out
+
+class ZeroLayer(nn.Module):
+    
+    def __init__(self, stride=None):
+        super(ZeroLayer, self).__init__()
+        self.stride = stride
+
+    def forward(self, x):
+        '''n, c, h, w = x.size()
+        h //= self.stride
+        w //= self.stride
+        device = x.get_device() if x.is_cuda else torch.device('cpu')
+        # noinspection PyUnresolvedReferences
+        padding = torch.zeros(n, c, h, w, device=device, requires_grad=False)
+        return padding'''
+        return x * 0
+
+    @staticmethod
+    def is_zero_layer():
+        return True
