064a9cd19441af4ed0d5a4330e5c1166d8f3be10
marsggbo <1435679023@qq.com>
2021-06-21 22:41:18 +0800

    add config for classify task, using DartsNetwork

--- hyperbox/models/classify_model.py
+++ hyperbox/models/classify_model.py
@@ -2,6 +2,7 @@ from typing import Any, List, Optional, Union
 
 import hydra
 import torch
+import numpy as np
 from omegaconf import DictConfig
 from pytorch_lightning import LightningModule
 from torchmetrics.classification.accuracy import Accuracy
@@ -65,31 +66,35 @@ class ClassifyModel(BaseModel):
 
         # log train metrics
         acc = self.train_metric(preds, targets)
-        self.log("train/loss", loss, on_step=True, on_epoch=True, prog_bar=False)
-        self.log("train/acc", acc, on_step=True, on_epoch=True, prog_bar=False)
+        self.log("train/loss", loss, on_step=True, on_epoch=True, sync_dist=True, prog_bar=False)
+        self.log("train/acc", acc, on_step=True, on_epoch=True, sync_dist=True, prog_bar=False)
 
         # we can return here dict with any tensors
         # and then read it in some callback or in training_epoch_end() below
         # remember to always return loss from training_step, or else backpropagation will fail!
-        return {"loss": loss, "preds": preds, "targets": targets}
+        return {"loss": loss, "preds": preds, "targets": targets, 'acc': acc}
 
     def training_epoch_end(self, outputs: List[Any]):
-        # `outputs` is a list of dicts returned from `training_step()`
-        pass
+        acc = np.mean([output['acc'].item() for output in outputs])
+        loss = np.mean([output['loss'].item() for output in outputs])
+        mflops, size = self.arch_size((1,3,32,32), convert=True)
+        logger.info(f"[rank {self.rank}] Train epoch{self.current_epoch} final result: loss={loss}, acc={acc}")
 
     def validation_step(self, batch: Any, batch_idx: int):
         loss, preds, targets = self.step(batch)
 
         # log val metrics
         acc = self.val_metric(preds, targets)
-        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
-        self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("val/loss", loss, on_step=False, on_epoch=True, sync_dist=True, prog_bar=False)
+        self.log("val/acc", acc, on_step=False, on_epoch=True, sync_dist=True, prog_bar=False)
 
-        return {"loss": loss, "preds": preds, "targets": targets}
+        return {"loss": loss, "preds": preds, "targets": targets, 'acc': acc}
 
     def validation_epoch_end(self, outputs: List[Any]):
-        pass
-
+        acc = np.mean([output['acc'].item() for output in outputs])
+        loss = np.mean([output['loss'].item() for output in outputs])
+        logger.info(f"[rank {self.rank}] Val epoch{self.current_epoch} final result: loss={loss}, acc={acc}")
+   
     def test_step(self, batch: Any, batch_idx: int):
         loss, preds, targets = self.step(batch)
 
@@ -101,4 +106,15 @@ class ClassifyModel(BaseModel):
         return {"loss": loss, "preds": preds, "targets": targets}
 
     def test_epoch_end(self, outputs: List[Any]):
-        pass
+        acc = np.mean([output['acc'].item() for output in outputs])
+        loss = np.mean([output['loss'].item() for output in outputs])
+        logger.info(f"Test epoch{self.current_epoch} final result: loss={loss}, acc={acc}")
+
+    def on_fit_start(self):
+        mflops, size = self.arch_size((1,3,32,32), convert=True)
+        logger.info(f"[rank {self.rank}] current model({self.arch}): {mflops:.4f} MFLOPs, {size:.4f} MB.")
+        
+    def on_fit_end(self):
+        mflops, size = self.arch_size((1,3,32,32), convert=True)
+        logger.info(f"[rank {self.rank}] current model({self.arch}): {mflops:.4f} MFLOPs, {size:.4f} MB.")
+        
