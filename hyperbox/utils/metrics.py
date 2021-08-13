b8e763d09a271955582a3ae639afffc7a6bcfa40
marsggbo <1435679023@qq.com>
2021-08-13 08:30:03 -0700

    features: add `KendallTau` and `Pearson` metrics

--- hyperbox/utils/metrics.py
+++ hyperbox/utils/metrics.py
@@ -1,5 +1,5 @@
 import torch
-
+import scipy.stats as stats
 
 class Accuracy:
     def __call__(self, output, target, topk=(1,)):
@@ -22,3 +22,28 @@ def accuracy(output, target, topk=(1,)):
     if len(res) == 1:
         return res[-1]
     return res
+
+class Pearson:
+    def __call__(self, output, target):
+        return pearson(output, target)
+
+def pearson(vector1, vector2):
+    n = len(vector1)
+    #simple sums
+    sum1 = sum(float(vector1[i]) for i in range(n))
+    sum2 = sum(float(vector2[i]) for i in range(n))
+    #sum up the squares
+    sum1_pow = sum([pow(v, 2.0) for v in vector1])
+    sum2_pow = sum([pow(v, 2.0) for v in vector2])
+    #sum up the products
+    p_sum = sum([vector1[i]*vector2[i] for i in range(n)])
+    #分子num，分母den
+    num = p_sum - (sum1*sum2/n)
+    den = math.sqrt((sum1_pow-pow(sum1, 2)/n)*(sum2_pow-pow(sum2, 2)/n))
+    if den == 0:
+        return 0.0
+    return num/den
+
+class KendallTau:
+    def __call__(self, output, target):
+        stats.kendalltau(output, target)
