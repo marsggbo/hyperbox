628d3b5bd97586a758664a02e70052a47bae9b94
marsggbo <1435679023@qq.com>
2021-09-12 20:22:33 -0700

    add lr scheduler&torch models

--- hyperbox/schedulers/warmup_scheduler.py
+++ hyperbox/schedulers/warmup_scheduler.py
@@ -18,7 +18,7 @@ class GradualWarmupScheduler(_LRScheduler):
         after_scheduler: after target_epoch, use this scheduler(eg. ReduceLROnPlateau)
     """
 
-    def __init__(self, optimizer, multiplier: float, warmup_epoch: int, after_scheduler=None):
+    def __init__(self, optimizer, multiplier: float=0.9, warmup_epoch: int, after_scheduler=None):
         self.multiplier = multiplier
         if self.multiplier < 1.:
             raise ValueError('multiplier should be greater thant or equal to 1.')
@@ -31,7 +31,7 @@ class GradualWarmupScheduler(_LRScheduler):
         super(GradualWarmupScheduler, self).__init__(optimizer)
 
     def get_lr(self):
-        if self.last_epoch > self.warmup_epoch:
+        if self.last_epoch >= self.warmup_epoch:
             if self.after_scheduler:
                 if not self.finished:
                     self.after_scheduler.base_lrs = [base_lr * self.multiplier for base_lr in self.base_lrs]
