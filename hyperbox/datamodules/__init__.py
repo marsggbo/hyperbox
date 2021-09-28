8102a09b504f8c9646c532d0f5b223988bbd63cb
marsggbo <1435679023@qq.com>
2021-09-27 20:06:08 -0700

    refactor datamodules __init__.py

--- hyperbox/datamodules/__init__.py
+++ hyperbox/datamodules/__init__.py
@@ -1,4 +1,7 @@
 from .cifar_datamodule import *
 from .fakedata_datamodule import *
 from .mnist_datamodule import *
-from .imagenet_datamodule import *
+
+from hyperbox.utils.utils import _module_available
+if _module_available("nvidia.dali"):
+    from .imagenet_datamodule import *
