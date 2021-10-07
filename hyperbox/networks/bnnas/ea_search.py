71d2f4a5da15706be9a9d7a29bc9ece3c320f67a
marsggbo <1435679023@qq.com>
2021-10-06 19:43:17 -0700

    update

--- hyperbox/networks/bnnas/ea_search.py
+++ hyperbox/networks/bnnas/ea_search.py
@@ -13,7 +13,8 @@ from hyperbox.mutator.utils import NonDominatedSorting
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     net = BNNet(num_classes=10, search_depth=False)
-    ckpt = '/home/xihe/xinhe/hyperbox/logs/runs/bnnas_c10_all_depth_adam0.001_sync_hete/2021-10-02_23-05-43/checkpoints/epoch=339_val/acc=43.1300.ckpt'
+    ckpt = '/home/xihe/xinhe/hyperbox/logs/runs/bnnas_c10_bn_depth_adam0.001_sync_hete/2021-10-02_23-05-51/checkpoints/epoch=390_val/acc=27.8100.ckpt'
+    # ckpt = '/home/xihe/xinhe/hyperbox/logs/runs/bnnas_c10_all_depth_adam0.001_sync_hete/2021-10-02_23-05-43/checkpoints/epoch=339_val/acc=43.1300.ckpt'
     ckpt = torch.load(ckpt, map_location='cpu')
     weights = {}
     for key in ckpt['state_dict']:
@@ -23,7 +24,7 @@ if __name__ == '__main__':
     net = net.to(device)
 
     # method 1
-    ea = EAMutator(net, num_population=50, algorithm='cars')
+    ea = EAMutator(net, num_population=50, algorithm='top')
     # ea.load_ckpt('epoch2.pth')
     eval_func = lambda arch, net: net.bn_metrics().item()
     ea.search(20, eval_func, verbose=True, filling_history=True)
@@ -34,7 +35,10 @@ if __name__ == '__main__':
     epoch = ea.crt_epoch
     pareto_lists = NonDominatedSorting(np.vstack( (size.reshape(-1), 1/metric.reshape(-1)) ))
     pareto_indices = pareto_lists[0] # e.g., [75,  87, 113, 201, 205]
-    plot_pareto_fronts(size, metric, pareto_indices, 'model size (MB)', 'BN-based metric', figname=f'pareto_epoch{epoch}.pdf')
+    plot_pareto_fronts(
+        size, metric, pareto_indices, 'model size (MB)', 'BN-based metric',
+        figname=f'bn_depth_pareto_epoch_single{epoch}.pdf'
+    )
 
     # method 2
     # ea = EAMutator(net, num_population=50, algorithm='top')
