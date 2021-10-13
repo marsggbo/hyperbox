e345f49efded6de0447781ff1c477ea11ad0a299
marsggbo <1435679023@qq.com>
2021-10-12 20:00:18 -0700

    update EASearch for BN-NAS

--- hyperbox/networks/bnnas/ea_search.py
+++ hyperbox/networks/bnnas/ea_search.py
@@ -13,8 +13,10 @@ from hyperbox.mutator.utils import NonDominatedSorting
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     net = BNNet(num_classes=10, search_depth=False)
-    ckpt = '/home/xihe/xinhe/hyperbox/logs/runs/bnnas_c10_bn_depth_adam0.001_sync_hete/2021-10-02_23-05-51/checkpoints/epoch=390_val/acc=27.8100.ckpt'
+    # ckpt = '/home/xihe/xinhe/hyperbox/logs/runs/bnnas_c10_bn_depth_adam0.001_sync_hete/2021-10-02_23-05-51/checkpoints/epoch=390_val/acc=27.8100.ckpt'
     # ckpt = '/home/xihe/xinhe/hyperbox/logs/runs/bnnas_c10_all_depth_adam0.001_sync_hete/2021-10-02_23-05-43/checkpoints/epoch=339_val/acc=43.1300.ckpt'
+    # ckpt = '/home/xihe/xinhe/hyperbox/logs/runs/bnnas_c10_bn_adam0.001_sync_hete/2021-10-06_06-29-41/checkpoints/epoch=392_val/acc=28.8200.ckpt'
+    ckpt = '/home/xihe/xinhe/hyperbox/logs/runs/bnnas_c10_all_adam0.001_sync_hete/2021-10-06_06-31-00/checkpoints/epoch=302_val/acc=44.0300.ckpt'
     ckpt = torch.load(ckpt, map_location='cpu')
     weights = {}
     for key in ckpt['state_dict']:
@@ -24,10 +26,12 @@ if __name__ == '__main__':
     net = net.to(device)
 
     # method 1
-    ea = EAMutator(net, num_population=50, algorithm='top')
+    mode = 'all'
+    search_algorithm = 'cars'
+    ea = EAMutator(net, num_population=50, algorithm=search_algorithm)
     # ea.load_ckpt('epoch2.pth')
     eval_func = lambda arch, net: net.bn_metrics().item()
-    ea.search(20, eval_func, verbose=True, filling_history=True)
+    ea.search(21, eval_func, verbose=True, filling_history=True)
     size = np.array([pool['size'] for pool in ea.history.values()])
     metric = np.array([pool['metric'] for pool in ea.history.values()])
     indices = np.argsort(size)
@@ -37,7 +41,7 @@ if __name__ == '__main__':
     pareto_indices = pareto_lists[0] # e.g., [75,  87, 113, 201, 205]
     plot_pareto_fronts(
         size, metric, pareto_indices, 'model size (MB)', 'BN-based metric',
-        figname=f'bn_depth_pareto_epoch_single{epoch}.pdf'
+        figname=f'{mode}_pareto_searchepoch{epoch}_{search_algorithm}.pdf'
     )
 
     # method 2
