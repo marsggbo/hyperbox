c9a94c5edca212b49a8669467d96637ed7f2d04d
marsggbo <1435679023@qq.com>
2021-12-07 00:04:14 -0800

    refactor: import `medmnist`

--- hyperbox/datamodules/__init__.py
+++ hyperbox/datamodules/__init__.py
@@ -1,8 +1,9 @@
+from hyperbox.utils.utils import _module_available
 from .cifar_datamodule import *
 from .fakedata_datamodule import *
 from .mnist_datamodule import *
-from .medmnist_datamodule import *
 
-from hyperbox.utils.utils import _module_available
+if _module_available("medmnist"):
+    from .medmnist_datamodule import *
 if _module_available("nvidia.dali"):
     from .imagenet_datamodule import *
