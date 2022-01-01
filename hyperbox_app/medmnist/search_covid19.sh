8428e44876961115859d6d4a6a41249ee6f1f1b6
marsggbo <1435679023@qq.com>
2022-01-01 20:59:26 +0800

    update default search bash

--- hyperbox_app/medmnist/search_covid19.sh
+++ hyperbox_app/medmnist/search_covid19.sh
@@ -4,16 +4,19 @@ exp=$1
 # gdas_iran
 others=$2
 
-CUDA_VISIBLE_DEVICES=0,1 python run.py \
+CUDA_VISIBLE_DEVICES=1 python run.py \
 hydra.searchpath=[file:///home/comp/18481086/code/hyperbox/hyperbox_app/medmnist/configs] \
 experiment=$exp \
-datamodule.img_size=[512,512] \
-datamodule.center_size=[360,360] \
+datamodule.img_size=[160,160] \
+datamodule.center_size=[120,120] \
 datamodule.slice_num=32 \
-datamodule.num_workers=4 \
-datamodule.batch_size=16 \
+datamodule.num_workers=3 \
+datamodule.batch_size=64 \
+model/optimizer_cfg=sgd \
+model.optimizer_cfg.lr=0.025 \
 model.metric_cfg._target_=hyperbox.utils.metrics.Accuracy \
-trainer.gpus=2 \
+trainer.gpus=1 \
+trainer.accelerator=dp \
 $others
 
 # experiment=gdas_ccccii.yaml \
