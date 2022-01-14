9375df09a5c0e5e53f240e804401c3b03a7ac7e1
marsggbo <1435679023@qq>
2022-01-14 06:49:47 -0800

    support LightningLite

--- hyperbox/lites/base_lite.py
+++ hyperbox/lites/base_lite.py
@@ -1,27 +1,22 @@
-from typing import List, Optional, Union, Any
+import os
+from typing import Any, Dict, List, Optional, Union
 
+import hydra
 import torch
+from torch.nn import Module
+from hydra.utils import instantiate
+from loguru import logger
 from omegaconf import DictConfig
 from pytorch_lightning.lite import LightningLite
-from pytorch_lightning.loops import Loop
 
 from hyperbox.models.base_model import BaseModel
-from hyperbox.utils.logger import get_logger
+from hyperbox.utils.utils import DotDict, hparams_wrapper
 
 
-class HyperboxLite(BaseModel, LightningLite):
+@hparams_wrapper
+class HyperboxLite(LightningLite):
     def __init__(
         self,
-        # LightningLite
-        accelerator: Optional[Union[str, "Accelerator"]] = None,
-        strategy: Optional[Union[str, "Strategy"]] = None,
-        devices: Optional[Union[List[int], str, int]] = None,
-        num_nodes: int = 1,
-        precision: Union[int, str] = 32,
-        plugins: Optional[Union["PLUGIN_INPUT", List["PLUGIN_INPUT"]]] = None,
-        gpus: Optional[Union[List[int], str, int]] = None,
-        tpu_cores: Optional[Union[List[int], str, int]] = None,
-
         # LightningModule
         network_cfg: Optional[Union[DictConfig, dict]] = None,
         mutator_cfg: Optional[Union[DictConfig, dict]] = None,
@@ -29,39 +24,90 @@ class HyperboxLite(BaseModel, LightningLite):
         loss_cfg: Optional[Union[DictConfig, dict]] = None,
         metric_cfg: Optional[Union[DictConfig, dict]] = None,
         scheduler_cfg: Optional[Union[DictConfig, dict]] = None,
+        # DataModule
         datamodule_cfg: Optional[Union[DictConfig, dict]] = None,
+        # LightningLite
+        lite_cfg: Optional[Union[DictConfig, dict]] = None,
+        # logger
+        logger_cfg: Optional[Union[DictConfig, dict]] = None,
+        # Run config
         run_cfg: Optional[Union[DictConfig, dict]] = None,
         **kwargs
     ) -> None:
-        BaseModel.__init__(self, network_cfg, mutator_cfg, optimizer_cfg,
-            loss_cfg, metric_cfg, scheduler_cfg, **kwargs)
-        self.other_args = kwargs
+        # Init LightningLite
+        self.lite_cfg = self.parse_lite_cfg(lite_cfg)
+        cfg = dict(self.lite_cfg)
+        LightningLite.__init__(self, **cfg)
+
+        path = os.getcwd()
+        log_file = os.path.join(path, 'log.txt')
+        logger.add(log_file)
+
+        self.pl_model = BaseModel(network_cfg, mutator_cfg, optimizer_cfg,
+            loss_cfg, metric_cfg, scheduler_cfg)
+
+        # Init DataModule
+        self._datamodule = self.build_datamodule(datamodule_cfg)
+        logger.info('Building datamodule ...')
+
+        # Init logger
+        if logger_cfg is not None:
+            self._logger = hydra.utils.instantiate(logger_cfg)
+
         self.run_cfg = run_cfg
-        LightningLite.__init__(self, accelerator, strategy, devices, num_nodes,
-            precision, plugins, gpus, tpu_cores)
-        logger.info(f'Building HyperboxLite:')
-        self.build_datamodule(datamodule_cfg)
+        self.kwargs = DotDict(kwargs)
+
+    def parse_lite_cfg(self, lite_cfg=None) -> None:
+        '''
+        Args:
+            accelerator: Optional[Union[str, "Accelerator"]] = None, # gpu, cpu, ipu, tpu
+            strategy: Optional[Union[str, "Strategy"]] = None, # "dp", "ddp", "ddp_spawn", "tpu_spawn", "deepspeed", "ddp_sharded", or "ddp_sharded_spawn"
+            devices: Optional[Union[List[int], str, int]] = None,
+            num_nodes: int = 1,
+            precision: Union[int, str] = 32,
+            plugins: Optional[Union["PLUGIN_INPUT", List["PLUGIN_INPUT"]]] = None,
+            gpus: Optional[Union[List[int], str, int]] = None,
+            tpu_cores: Optional[Union[List[int], str, int]] = None,
+        '''
+        default_lite_cfg = {
+            'accelerator': None,
+            'strategy': None,
+            'devices': None,
+            'num_nodes': 1,
+            'precision': 32,
+            'plugins': None,
+            'gpus': None,
+            'tpu_cores': None,
+        }
+        default_lite_cfg = DotDict(default_lite_cfg)
+        for key, value in lite_cfg.items():
+            default_lite_cfg[key] = value
+        return default_lite_cfg
 
     def build_datamodule(self, datamodule_cfg: DictConfig) -> Any:
         self.datamodule_cfg = datamodule_cfg
         if datamodule_cfg is not None:
-            self.datamodule: LightningDataModule = hydra.utils.instantiate(datamodule_cfg)
+            datamodule = hydra.utils.instantiate(datamodule_cfg)
         else:
-            self.datamodule = None
+            datamodule = None
+        datamodule.setup()
+        return datamodule
 
-    def setup_dataloaders(self, datamodule) -> Any:
-        if datamodule is not None:
-            logger.info('1. setup_dataloaders')
-            dataloaders = {key: d for key, d in self.dataloaders.items() if d is not None}
-            keys = list(dataloaders.keys())
-            loaders = self.setup_dataloaders(*list(dataloaders.values()))
-            if len(loaders) == 3:
-                self._train_dataloader, self._val_dataloader, self._test_dataloader = loaders
-            elif len(loaders) == 2:
-                self._train_dataloader, self._test_dataloader = loaders
-        else:
-            logger.info('1. setup_dataloaders: no dataloaders found')
-            self._train_dataloader, self._val_dataloader, self._test_dataloader = None, None, None
+    @property
+    def datamodule(self):
+        return self._datamodule
+
+    @datamodule.setter
+    def datamodule(self, value):
+        self._datamodule = value
+
+    @property
+    def logger(self):
+        return self._logger
+
+    @logger.setter
+    def logger(self, value):
+        self._logger = value
 
     @property
     def dataloaders(self) -> Dict[str, Any]:
@@ -76,7 +122,7 @@ class HyperboxLite(BaseModel, LightningLite):
         if getattr(self, '_train_dataloader', None) is not None:
             return self._train_dataloader
         elif self.datamodule is not None and hasattr(self.datamodule, 'train_dataloader'):
-            return self.datamodule.train_dataloader
+            return self.datamodule.train_dataloader()
         else:
             return None
 
@@ -89,7 +135,7 @@ class HyperboxLite(BaseModel, LightningLite):
         if getattr(self, '_val_dataloader', None) is not None:
             return self._val_dataloader
         elif self.datamodule is not None and hasattr(self.datamodule, 'val_dataloader'):
-            return self.datamodule.val_dataloader
+            return self.datamodule.val_dataloader()
         else:
             return None
 
@@ -102,41 +148,10 @@ class HyperboxLite(BaseModel, LightningLite):
         if getattr(self, '_test_dataloader', None) is not None:
             return self._test_dataloader
         elif self.datamodule is not None and hasattr(self.datamodule, 'test_dataloader'):
-            return self.datamodule.test_dataloader
+            return self.datamodule.test_dataloader()
         else:
             return None
 
     @test_dataloader.setter
     def test_dataloader(self, loader):
         self.test_dataloader = loader
-
-    def run(self, *args, **kwargs) -> Any:
-        ...
-        ''' 
-        #1. setup_dataloaders
-        if self.datamodule is None:
-            # manual dataloaders
-            train_loader, test_loader = self.setup_dataloaders(self.train_loader, self.test_loader)
-        else:
-            train_loader, test_loader = self.train_loader, self.test_loader
-
-        #2. setup_network
-        model = self.network
-        optimizer = self.optimizers()
-        scheduler = self.lr_schedulers()
-
-        model, optimizer = self.setup(model, optimizer)
-        scheduler = StepLR(optimizer, step_size=1, gamma=hparams.gamma)
-
-        MainLoop(self, model, optimizer, scheduler, train_loader, test_loader, hparams).run()
-
-        if hparams.save_model and self.is_global_zero:
-            self.save(model.state_dict(), "mnist_cnn.pt")
-        '''
-
-
-if __name__ == "__main__":
-    from omegaconf import OmegaConf
-    cfg = OmegaConf.load("/home/xihe/xinhe/hyperbox/logs/runs/bnnas/bnnas_c10_all_adam0.001_sync_hete/2021-10-06_06-31-00/.hydra/config.yaml")
-    cfg_model = cfg.model
-    lite = HyperboxLite(**cfg_model)
