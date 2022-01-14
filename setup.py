5177edd5d23a0b8b77a276b00a507629cca67b40
marsggbo <1435679023@qq>
2022-01-13 21:13:59 -0800

    v1.0

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyperbox",  # you should change "src" to your project name
-    version="0.0.1",
+    version="1.0",
     description="Hyperbox: An easy-to-use NAS framework.",
     author="marsggbo",
     # replace with your own github project link
-    install_requires=["pytorch-lightning>=1.2.0", "hydra-core>=1.0.6"],
+    install_requires=["pytorch-lightning>=1.5", "hydra-core>=1.1"],
     packages=find_packages(),
 )
