8e24a22c35943aa628d2a009d387585426635e5d
marsggbo <1435679023@qq>
2022-01-15 23:54:08 -0800

    distribute v1.0.0

--- distribute.sh
+++ distribute.sh
@@ -1,9 +1,16 @@
-rm -rf build dist *.egg-info
+#!/bin/bash
+
+rm -rf build dist hyperbox.egg-info
 python setup.py sdist bdist_wheel
+echo Building...
+echo Uploading...
 if (($1==1));then
     python -m twine upload dist/*
+    echo Uploading to PyPI... 
 elif (($1==2));then
-    python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+    python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/* 
+    echo Uploading to TestPyPI...
 else
     echo "Wrong command, only support 1 or 2"
 fi
+echo Done.
--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyperbox",  # you should change "src" to your project name
-    version="1.0",
+    version="1.0.0",
     description="Hyperbox: An easy-to-use NAS framework.",
     author="marsggbo",
+    url="https://github.com/marsggbo/hyperbox",
     # replace with your own github project link
     install_requires=["pytorch-lightning>=1.5", "hydra-core>=1.1"],
     packages=find_packages(),
