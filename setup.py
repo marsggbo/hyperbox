b899de75c9efe0c59b521cec41cce5159f18c1a7
marsggbo <1435679023@qq>
2022-04-18 04:31:59 -0700

    v1.1.1 fix bug of packging (include cfg files)

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name="hyperbox",  # you should change "src" to your project name
-    version="1.1.0",
+    version="1.1.1",
     description="Hyperbox: An easy-to-use NAS framework.",
     author="marsggbo",
     url="https://github.com/marsggbo/hyperbox",
     # replace with your own github project link
     install_requires=["pytorch-lightning>=1.5", "hydra-core>=1.1"],
     packages=find_packages(),
+    include_package_data=True,
 )
