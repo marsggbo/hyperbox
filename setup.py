d3eab10823750d188c66f34b347cda0d1139e4cf
marsggbo <1435679023@qq>
2022-04-27 06:52:12 -0700

    v1.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hyperbox",  # you should change "src" to your project name
-    version="1.1.2",
+    version="1.1.3",
     description="Hyperbox: An easy-to-use NAS framework.",
     author="marsggbo",
     url="https://github.com/marsggbo/hyperbox",
