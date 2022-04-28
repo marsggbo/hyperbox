7e9a4815a7b1914ae7b2e72e86556ca37bc9445b
marsggbo <1435679023@qq>
2022-04-27 23:46:38 -0700

    fix typo

--- hyperbox/mutator/random_multiple_mutator.py
+++ hyperbox/mutator/random_multiple_mutator.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 
-from hyperbox.mutables.spaces import InputSpace, OperationSpace, ValueSpace\
+from hyperbox.mutables.spaces import InputSpace, OperationSpace, ValueSpace
 from hyperbox.mutator.default_mutator import Mutator
 from hyperbox.mutator.random_mutator import RandomMutator
 
