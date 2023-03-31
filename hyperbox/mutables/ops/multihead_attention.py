81582df2e9e7e25b21d9353423d9c625a83fbd27
marsggbo <1435679023@qq>
2023-03-30 22:38:48 -0700

    add poolconv-based transformation emthod

--- hyperbox/mutables/ops/multihead_attention.py
+++ hyperbox/mutables/ops/multihead_attention.py
@@ -80,14 +80,76 @@ class MultiheadAttention(FinegrainedModule):
         for i in range(len(embed_dim_candidates) - 1):
             dim_small = embed_dim_candidates[i]
 
-            # weiights
-            if self.transform_params_method == 'Large2Small':
+            if self.transform_params_method == 'PoolConv':
+                assert max_embed_dim % dim_small == 0, \
+                    f'embed_dim ({max_embed_dim}) must be divisible by candidate dim ({dim_small})'
+                downsample_ratio = max_embed_dim // dim_small
+                downsample_times = int(math.log2(downsample_ratio))
+                
+                # in_proj_weight
+                param_name = f'in_proj_weight_TM_{max_embed_dim}to{dim_small}'
+                matrices[param_name] = nn.Sequential(
+                    *[nn.Sequential(
+                        # nn.AvgPool2d(2),
+                        nn.Conv2d(1, 1, 3, 2, 1, bias=False)
+                    ) for i in range(downsample_times)],
+                )
+
+                # out_proj_weight
+                param_name = f'out_proj_weight_TM_{max_embed_dim}to{dim_small}'
+                matrices[param_name] = nn.Sequential(
+                    *[nn.Sequential(
+                        # nn.AvgPool2d(2),
+                        nn.Conv2d(1, 1, 3, 2, 1, bias=False)
+                    ) for i in range(downsample_times)],
+                )
+
+                # in_proj_bias
+                if self.in_proj_bias is not None:
+                    param_name = f'in_proj_bias_TM_{max_embed_dim}to{dim_small}'
+                    matrices[param_name] = nn.Sequential(
+                        *[nn.Sequential(
+                            # nn.AvgPool1d(2),
+                            nn.Conv1d(1, 1, 3, 2, 1, bias=False)
+                        ) for i in range(downsample_times)]
+                    )
+                
+                # out_proj_bias
+                if self.out_proj.bias is not None:
+                    param_name = f'out_proj_bias_TM_{max_embed_dim}to{dim_small}'
+                    matrices[param_name] = nn.Sequential(
+                        *[nn.Sequential(
+                            # nn.AvgPool1d(2),
+                            nn.Conv1d(1, 1, 3, 2, 1, bias=False)
+                        ) for i in range(downsample_times)]
+                    )
+
+                # bias_k
+                if self.bias_k is not None:
+                    param_name = f'bias_k_TM_{max_embed_dim}to{dim_small}'
+                    matrices[param_name] = nn.Sequential(
+                        *[nn.Sequential(
+                            # nn.AvgPool1d(2),
+                            nn.Conv1d(1, 1, 3, 2, 1, bias=False)
+                        ) for i in range(downsample_times)]
+                    )
+
+                # bias_v
+                if self.bias_v is not None:
+                    param_name = f'bias_v_TM_{max_embed_dim}to{dim_small}'
+                    matrices[param_name] = nn.Sequential(
+                        *[nn.Sequential(
+                            # nn.AvgPool1d(2),
+                            nn.Conv1d(1, 1, 3, 2, 1, bias=False)
+                        ) for i in range(downsample_times)]
+                    )
+            elif self.transform_params_method == 'Large2Small':
                 # in_proj_weight
                 left_param_name = f'in_proj_weight_left_TM_{max_embed_dim}to{dim_small}'
                 right_param_name = f'in_proj_weight_right_TM_{max_embed_dim}to{dim_small}'
-                matrices[left_param_name] = Parameter(torch.eye(3*dim_small, 3*max_embed_dim))
-                matrices[right_param_name] = Parameter(torch.eye(max_embed_dim, dim_small))               
-                
+                matrices[left_param_name] = Parameter(torch.eye(dim_small, 3*max_embed_dim))
+                matrices[right_param_name] = Parameter(torch.eye(max_embed_dim, 3*dim_small))
+
                 # out_proj_weight
                 left_param_name = f'out_proj_weight_left_TM_{max_embed_dim}to{dim_small}'
                 right_param_name = f'out_proj_weight_right_TM_{max_embed_dim}to{dim_small}'
@@ -116,11 +178,11 @@ class MultiheadAttention(FinegrainedModule):
             elif self.transform_params_method == 'TruncatedLinear':
                 # in_proj_weight
                 param_name = f'in_proj_weight_TM_{dim_small}'
-                matrices[param_name] = Parameter(torch.eye(3*dim_small, 3*dim_small))
+                matrices[param_name] = Parameter(torch.eye(dim_small, dim_small))
 
                 # out_proj_weight
                 param_name = f'out_proj_weight_TM_{dim_small}'
-                matrices[param_name] = Parameter(torch.eye(dim_small, max_embed_dim))
+                matrices[param_name] = Parameter(torch.eye(dim_small, dim_small))
 
                 # in_proj_bias
                 if self.in_proj_bias is not None:
@@ -144,8 +206,11 @@ class MultiheadAttention(FinegrainedModule):
             else:
                 raise NotImplementedError
                 
-        for name, param in matrices.items():
-            self.register_parameter(name, param)
+        for name, val in matrices.items():
+            if isinstance(val, Parameter):
+                self.register_parameter(name, val)
+            elif isinstance(val, nn.Module):
+                self.add_module(name, val)
 
     def forward(
         self,
@@ -173,8 +238,7 @@ class MultiheadAttention(FinegrainedModule):
                 training=self.training,
                 key_padding_mask=key_padding_mask, need_weights=need_weights,
                 attn_mask=attn_mask)
-        else:
-            
+        else:            
             if self.search_num_heads:
                 num_heads = self.value_spaces['num_heads'].value
             else:
@@ -201,12 +265,31 @@ class MultiheadAttention(FinegrainedModule):
     def transform_params(self, embed_dim):
         if embed_dim == self.embed_dim:
             return self.in_proj_weight, self.in_proj_bias, self.bias_k, self.bias_v, self.out_proj.weight, self.out_proj.bias
-        if self.transform_params_method == 'Large2Small':
+        if self.transform_params_method == 'PoolConv':
+            in_proj_weight = getattr(
+                self, f'in_proj_weight_TM_{self.embed_dim}to{embed_dim}'
+                )(self.in_proj_weight.unsqueeze(0).unsqueeze(0)).squeeze(0).squeeze(0)
+            out_proj_weight = getattr(
+                self, f'out_proj_weight_TM_{self.embed_dim}to{embed_dim}'
+                )(self.out_proj.weight.unsqueeze(0).unsqueeze(0)).squeeze(0).squeeze(0)
+            in_proj_bias = getattr(
+                self, f'in_proj_bias_TM_{self.embed_dim}to{embed_dim}'
+                )(self.in_proj_bias.unsqueeze(0)).squeeze(0) if self.in_proj_bias is not None else None
+            out_proj_bias = getattr(
+                self, f'out_proj_bias_TM_{self.embed_dim}to{embed_dim}'
+                )(self.out_proj.bias.unsqueeze(0)).squeeze(0) if self.out_proj.bias is not None else None
+            bias_k = getattr(
+                self, f'bias_k_TM_{self.embed_dim}to{embed_dim}'
+                )(self.bias_k.unsqueeze(0)).squeeze(0) if self.bias_k is not None else None
+            bias_v = getattr(
+                self, f'bias_v_TM_{self.embed_dim}to{embed_dim}'
+                )(self.bias_v.unsqueeze(0)).squeeze(0) if self.bias_v is not None else None
+        elif self.transform_params_method == 'Large2Small':
             # weights
             in_proj_weight = self.param_transform_via_TM(
                 self.in_proj_weight,
                 L_transform_matrix=getattr(self, f'in_proj_weight_left_TM_{self.embed_dim}to{embed_dim}'),
-                R_transform_matrix=getattr(self, f'in_proj_weight_right_TM_{self.embed_dim}to{embed_dim}'))
+                R_transform_matrix=getattr(self, f'in_proj_weight_right_TM_{self.embed_dim}to{embed_dim}')).T
             out_proj_weight = self.param_transform_via_TM(
                 self.out_proj.weight,
                 L_transform_matrix=getattr(self, f'out_proj_weight_left_TM_{self.embed_dim}to{embed_dim}'),
@@ -288,6 +371,7 @@ class MultiheadAttention(FinegrainedModule):
 
 if __name__ == '__main__':
     import timeit
+    from time import time
     from hyperbox.mutables.ops import Linear
     from hyperbox.networks.base_nas_network import BaseNASNetwork
     from hyperbox.mutator import RandomMutator
@@ -296,29 +380,68 @@ if __name__ == '__main__':
         def __init__(self, dim_in, dims, heads, transform_params_method='TruncatedLinear'):
             super().__init__()
             self.linear = Linear(dim_in, dims)
-            self.mha = MultiheadAttention(dims, heads, 0.1, True)
+            self.mha = MultiheadAttention(dims, heads, 0.1, True,
+                                          transform_params_method=transform_params_method)
+            self.linear_time = []
+            self.mha_time = []
         def forward(self, x):
+            start = time()
             x = self.linear(x)
+            end = time()
+            self.linear_time.append(end-start)
+            
+            start = time()
             y = self.mha(x, x, x)
+            end = time()
+            self.mha_time.append(end-start)
             return y[0]
 
-    for tpm in ['TruncatedLinear', 'Large2Small', 'disbale']:
-        print(f"...")
+    for tpm in [
+            'disable', 
+            'Large2Small', 
+            'PoolConv', 
+            'TruncatedLinear'
+        ]:
+        print(f"testing {tpm}...")
         dims = ValueSpace([256, 512, 1024])
         heads = ValueSpace([4, 8, 16])
-        device = 'mps'
+        # device = 'mps'
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
         dim_in = 128
         net = Net(dim_in, dims, heads, tpm).to(device)
+        total_params = sum(p.numel() for p in net.parameters())
+        if tpm == 'disable':
+            TM_params = 0
+        else:
+            TM_params = sum(p.numel() for name, p in net.named_parameters() if 'TM' in name)
+        print(f"Testing {tpm}: Total params: {total_params}, TM params: {TM_params} ({TM_params/total_params:.2%})")
         rm = RandomMutator(net)
-        x = torch.rand(2, 10, dim_in).to(device)
-        
-        for i in range(10):
-            rm.reset()
-            y = net(x)
-            # print(net.arch)
-            # print(y[0].shape)
+        x = torch.rand(16, 512, dim_in).to(device)
 
-        def test_forward():
-            net(x)
-        t = timeit.timeit(test_forward, number=200, globals=globals())
-        print(f"Testing {tpm}: Time taken about {t:.6f} seconds")
+        steps = 100
+        start = time()
+        reset_times = []
+        fw_times = []
+        bw_times = []
+        for i in range(steps):
+            start1 = time()
+            rm.reset()
+            end1 = time()
+            reset_times.append(end1-start1)
+            start2 = time()
+            y = net(x)
+            end2 = time()
+            fw_times.append(end2-start2)
+            start3 = time()
+            loss = y.sum()
+            loss.backward()
+            end3 = time()
+            bw_times.append(end3-start3)
+        end = time()
+        print(f"Testing {tpm}: Time taken about {(end-start)/(steps-5):.6f} seconds")
+        print(f"\treset time: {sum(reset_times[5:])/(steps-5):.6f}")
+        print(f"\tforward time: {sum(fw_times[5:])/(steps-5):.6f}")
+        print(f"\t\tlinear time: {sum(net.linear_time[5:])/(steps-5):.6f}")
+        print(f"\t\tmha time: {sum(net.mha_time[5:])/(steps-5):.6f}")
+        print(f"\tbackward time: {sum(bw_times[5:])/(steps-5):.6f}")
+        print('*'*20)
